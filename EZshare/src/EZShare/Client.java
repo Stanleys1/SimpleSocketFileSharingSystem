e0ed31050b066b6f6bb2d02f9ba20ad2d979c929
Stanley <stanley_sim1@hotmail.com>
2017-04-29 01:50:13 +1000

    small changes

--- EZshare/src/EZShare/Client.java
+++ EZshare/src/EZShare/Client.java
@@ -6,6 +6,16 @@ import java.io.IOException;
 public class Client {
 	
 	public static void main (String[] args){
+		//args must be present
+		if(args.length==0){
+			System.out.println("please enter one command");
+			System.exit(0);
+		}
+		//commands must start with -
+		if(!args[0].startsWith("-")){
+			System.out.println("commands for client must start with '-'");
+			System.exit(0);
+		}
 		
 		try{
 			EzClient client = new EzClient(args,true,false);
