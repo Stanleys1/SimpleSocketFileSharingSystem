ff18872cb99fe0304dc8ef9cd7b1174c794dab30
Stanley <stanley_sim1@hotmail.com>
2017-05-16 21:55:19 +1000

    match template

--- EZshare/src/EZShare/Resource.java
+++ EZshare/src/EZShare/Resource.java
@@ -165,10 +165,22 @@ public class Resource {
 		}
 		
 		if(!template.getName().equals("")){
-			return name.contains(template.getName());
-		}else if(!template.getDescription().equals("")){
-			return description.contains(template.getDescription());
-		}else return true;
+			if( name.contains(template.getName())){
+				return true;
+			}
+		}
+		
+		if(!template.getDescription().equals("")){
+			if(description.contains(template.getDescription())){
+				return true;
+			}
+		}
+		
+		if(template.getName().equals("")&&template.getName().equals("")){
+			return true;
+		}
+		
+		return false;
 	}
 	
 	/**
